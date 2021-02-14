157ddc193c7b3ee040527cffeb18bf2400617350
Natthaphon Fueangfung <natthaphon.f@psru.ac.th>
2021-02-14 16:37:04 +0700

    update bat

--- app/src/main/java/com/example/pong/Ball.java
+++ app/src/main/java/com/example/pong/Ball.java
@@ -12,8 +12,8 @@ public class Ball {
 
     Ball(int screenX){
 // Make the ball square and 1% of screen width of the screen width
-        mBallWidth = screenX / 10;
-        mBallHeight = screenX / 10;
+        mBallWidth = screenX / 50;
+        mBallHeight = screenX / 50;
 
 // Initialize the RectF with 0, 0, 0, 0
         mRect = new RectF();
